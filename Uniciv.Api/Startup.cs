14d8defa6fa8c7049e74a2f852412e63a41f6ad9
Melqz <melquizedes@gmail.com>
2020-10-22 14:32:58 -0300

    v2 API finalizada.

--- Uniciv.Api/Startup.cs
+++ Uniciv.Api/Startup.cs
@@ -21,6 +21,7 @@ namespace Uniciv.Api
 
             // Utilizando o modo Singleton
             services.AddSingleton<IFundoCapitalRepository, FundoCapitalRepository>(); 
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
